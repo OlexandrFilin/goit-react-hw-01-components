a361e2e9cfccb4b38efb86beb7a19641e0cf04ec
OleksandrFilin <acfiln@ukr.net>
2023-09-28 22:41:18 +0300

    All markup done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
 import { Profile } from './prfile';
-import dataUser from '../path/to/user.json'
+import dataUser from '../path/to/user.json';
 import { Statistics } from './Statistics';
-import statistictUser from "../path/to/data.json"
+import statistictUser from '../path/to/data.json';
+import { FriendList } from './FriendList/FriendList';
+import friends from '../path/to/friends.json';
+import { TransactionHistory } from './Transhistory/TransactionHistory';
+import transactions from '../path/to/transactions.json';
 
 const App = () => {
-
   return (
     <>
-    <Profile dataUser={ dataUser } />
-    <Statistics title= "Upload stats" stats ={ statistictUser}  />
+      <Profile dataUser={dataUser} />
+      <Statistics title="Upload stats" stats={statistictUser} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
 
-export {App} 
+export { App };
--- src/components/prfile.jsx
+++ src/components/prfile.jsx
@@ -1,17 +1,20 @@
-import { Stats } from "./stats"
-import { CardUser } from "./cardUser"
+import { Stats } from './stats';
+import { CardUser } from './CardUser/cardUser';
 
+const Profile = ({ dataUser }) => {
+  const { username, tag, location, avatar, stats } = dataUser;
+  const { followers, views, likes } = stats;
+  return (
+    <div className="profile">
+      <CardUser
+        username={username}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+      />
+      <Stats followers={followers} views={views} likes={likes} />
+    </div>
+  );
+};
 
-
- const Profile = ({dataUser}) => {
-     const { username, tag, location, avatar, stats } = dataUser;
-     const {followers, views, likes} = stats;
-    return (
-   < div className="profile" >
-     <CardUser   username={username} tag={tag}
-      location={location} avatar={avatar} />
-      <Stats followers={followers} views={views} likes={ likes} />
-</div >)
-} 
-
-export { Profile  }
+export { Profile };
