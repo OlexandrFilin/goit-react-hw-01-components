726f4979dcff8f393902005baa9a465142668975
OleksandrFilin <acfiln@ukr.net>
2023-09-28 17:04:24 +0300

    add markup statistic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import { Profile } from './prfile';
-import dataUser from '../data/user.json'
+import dataUser from '../path/to/user.json'
+import { Statistics } from './Statistics';
+import statistictUser from "../path/to/data.json"
 
 const App = () => {
 
   return (
+    <>
     <Profile dataUser={ dataUser } />
-    
+    <Statistics title= "Upload stats" stats ={ statistictUser}  />
+    </>
   );
 };
 
